4082ae781946d3b38f4044c01eb02206c9acef23
CanranBrassica <kyabetsu.3141@gmail.com>
2018-10-04 01:43:49 +0900

    fileから読み込むように

--- init.py
+++ init.py
@@ -8,14 +8,23 @@ is_first = True
 if is_first:
     WIKI_URL = "https://wiki.eeic.jp"
     API_URL = WIKI_URL + "/api.php"
-    bot_username = os.environ['BOT_USERNAME']
-    bot_password = os.environ['BOT_PASSWORD']
+    f = open("user_info.json")
+    import json
+    user_info = json.load(f)
+    f.close()
+    bot_username = user_info['BOT_USERNAME']
+    bot_password = user_info['BOT_PASSWORD']
+    api_token = user_info['API_TOKEN']
+
+    #bot_username = os.environ['BOT_USERNAME']
+    #bot_password = os.environ['BOT_PASSWORD']
+    #api_token = os.environ['API_TOKEN']
+
     session = wiki.MediaWikiSession(API_URL, bot_username, bot_password)
     page_name = 'EEIC2019/課題一覧'
 
     # slackBotとassingment_notify_mgrを用意
     # botを別スレッドでスタート
-    api_token = os.environ['API_TOKEN']
     bot = eeic_bot.SlackBot(API_TOKEN=api_token)
     assignment_notify_mgr = assignment_notify.AssignmentListMgr(session, page_name)
 
