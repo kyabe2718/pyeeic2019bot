c4d50b436ca109c4376810a9b87aae289278d51f
CanranBrassica <kyabetsu.3141@gmail.com>
2018-10-04 01:46:11 +0900

    ファイルから

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -2,9 +2,16 @@
 
 import os
 
-
 # botアカウントのトークンを指定
-API_TOKEN = os.environ['API_TOKEN']
+
+import json
+
+f = open("user_info.json")
+user_info = json.load(f)
+f.close()
+
+API_TOKEN = user_info['API_TOKEN']
+#API_TOKEN = os.environ'API_TOKEN']
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "何を言っているのかわからないよ？"
