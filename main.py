6b1ea3e35461c4f8f7a02bff97e65bb47edf5479
CanranBrassica <kyabetsu.3141@gmail.com>
2018-10-04 02:42:51 +0900

    global ip

--- main.py
+++ main.py
@@ -2,14 +2,17 @@
 
 import time
 import schedule
+import atexit
+import subprocess
 
 import init
-import atexit
 
 
 def main():
+
     # スケジューラにタスクを登録
     #   10分毎にassignment_notify_mgrを更新する
+
     schedule.every(10).minutes.do(init.assignment_notify_mgr.updateAssignmentList)
 
     schedule.every().day.at("17:00").do(init.postTommorowAssignment)
@@ -18,7 +21,10 @@ def main():
 
     atexit.register(init.atDeath)   # プログラム終了時に呼ばれる関数を登録
     init.bot.postMessage("#dev_bot" ,"生き返った！！")
-    print("all initialized")
+    print("all initialized\n")
+
+    res = subprocess.check_output(['curl', 'inet-ip.info'])
+    print("global ip is ", res)
 
     while True:
         schedule.run_pending()
