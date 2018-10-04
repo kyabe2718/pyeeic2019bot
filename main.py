d0f72dc7c37913ad80a30e9316c8b608e3fe9a97
CanranBrassica <kyabetsu.3141@gmail.com>
2018-10-04 10:28:11 +0900

    fix

--- main.py
+++ main.py
@@ -23,8 +23,9 @@ def main():
 
     schedule.every().saturday.at("17:00").do(init.postNextWeekAssignment)
 
-    atexit.register(lambda : init.bot('#dev_bot', "正常終了！！"))   # プログラム終了時に呼ばれる関数を登録
-    signal.signal(signal.SIGTERM, lambda signum , frame: init.bot.postMessage('#dev_bot', "signalに殺された！！ signum: "+ str(signum)))
+    atexit.register(lambda : init.bot.postMessage('#dev_bot', "正常終了！！"))   # プログラム終了時に呼ばれる関数を登録
+    signal.signal(signal.SIGTERM, lambda signum , frame: init.bot.postMessage('#dev_bot', "signalに殺された！！ signum: "+ str(signum); sys.exit(0))
+    signal.signal(signal.SIGINT, lambda signum , frame: init.bot.postMessage('#dev_bot', "signalに殺された！！ signum: "+ str(signum); sys.exit(0)))
 
     init.bot.postMessage("#dev_bot" ,"生き返った！！\nglobal ip is " + res.decode('utf-8'))
     print("all initialized\n")
